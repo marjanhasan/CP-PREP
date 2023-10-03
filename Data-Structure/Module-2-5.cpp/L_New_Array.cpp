578d37f20be1aff63afa259a6c61bf5f2be02a44
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-03 16:08:48 +0600

    L-New Array solved

---
+++ Data-Structure/Module-2-5.cpp/L_New_Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    for (int i = 0; i < n; i++)
+        b.push_back(a[i]);
+    for (int val : b)
+        cout << val << " ";
+    return 0;
+}
