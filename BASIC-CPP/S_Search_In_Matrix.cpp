ff998152c29b475e45d3f28ab18f11a23a15d34a
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-20 02:27:58 +0600

    Module 18.5 problem 7 solved

---
+++ BASIC-CPP/S_Search_In_Matrix.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cin >> arr[i][j];
+    }
+    int x;
+    cin >> x;
+    bool flag = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == x)
+            {
+                flag = true;
+            }
+        }
+    }
+    flag ? cout << "will not take number" : cout << "will take number";
+    return 0;
+}
