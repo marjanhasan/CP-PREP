298574d27a2bc44675de737cff2269aaef154442
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-21 04:25:49 +0600

    operations recap done

---
+++ Data-Structure/List/operations.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l = {10, 20, 60, 20, 10, 30, 10};
+    // l.remove(10); // jto gula 10 so remove
+
+    // l.sort(); // ascending order
+    // l.sort(greater<int>()); // descending order
+
+    // unique krar way
+    // l.sort();
+    // l.sort(greater<int>());
+    // l.unique(); // sort kre nitei hbe
+
+    l.reverse();
+    for (int v : l)
+        cout << v << " ";
+    return 0;
+}
