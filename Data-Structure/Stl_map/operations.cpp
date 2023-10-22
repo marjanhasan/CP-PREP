c1e40fdc6ee70b38420d5f7043d584a9b3f5aa2e
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-22 21:57:54 +0600

    Stl map recap done

---
+++ Data-Structure/Stl_map/operations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string, int> mp;
+    mp.insert({"sakib", 75});
+    cout << mp["sakib"] << endl;
+    // most practice way to insert
+    mp["rakib"] = 17;                                // O(logN)
+    for (auto it = mp.begin(); it != mp.end(); it++) // O(NlogN)
+    {
+        cout << it->first << " " << it->second << endl; // O(logN)
+    }
+    if (mp.count("Sakib"))
+        cout << "Ase";
+    else
+        cout << "Nai";
+    return 0;
+}
