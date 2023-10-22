4d38ed650a9c8c68d1ea21849923f814b972de6f
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-22 14:15:44 +0600

    Priority Queue or Heap sort recap done

---
+++ Data-Structure/Stl_priority_queue_Heap_sort/operations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Heap sort, Priority Queue
+    // kono value insert/ delete krar por sort krar drkr hle
+    // priority_queue<int> pq; // max heap
+    priority_queue<int, vector<int>, greater<int>> pq; // min heap
+    while (true)
+    {
+        int c;
+        cin >> c;
+        if (c == 0)
+        {
+            int v;
+            cin >> v;
+            pq.push(v); // O(logN)
+        }
+        else if (c == 1)
+            pq.pop(); // O(logN)
+        else if (c == 2)
+            cout << pq.top() << endl; // O(1)
+        else
+            break;
+    }
+
+    return 0;
+}
