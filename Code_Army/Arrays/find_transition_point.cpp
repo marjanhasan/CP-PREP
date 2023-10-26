93f3fc7d337e380054c040239e349f4efc9afb4d
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-27 03:18:59 +0600

    find transition point

---
+++ Code_Army/Arrays/find_transition_point.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int transitionPoint(int arr[], int n);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+// ********************** O(logN)
+int transitionPoint(int arr[], int n)
+{
+    // code here
+    int l = 0, r = n - 1;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (arr[mid] == 0)
+            l = mid + 1;
+        else
+        {
+            if (arr[mid - 1] == 0)
+                return mid;
+            else
+                r = mid - 1;
+        }
+    }
+    return -1;
+}
