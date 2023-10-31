9181f1666e76d6496fc61526a22ff97b510cd508
marjanhasan <moonmarjanhasan@gmail.com>
2023-10-31 18:03:21 +0600

    max and min added

--- BASIC-CPP/K_Max_and_Min.cpp
+++ BASIC-CPP/K_Max_and_Min.cpp
@@ -4,17 +4,48 @@ int main()
 {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a <= b && a <= c)
-        cout << a << " ";
-    else if (b <= a && b <= c)
-        cout << b << " ";
-    else
-        cout << c << " ";
+    int mn, mx;
     if (a >= b && a >= c)
-        cout << a;
+    {
+        mx = a;
+        if (b > c)
+        {
+            mn = c;
+            cout << mn << " " << mx;
+        }
+        else
+        {
+            mn = b;
+            cout << mn << " " << mx;
+        }
+    }
     else if (b >= a && b >= c)
-        cout << b;
+    {
+        mx = b;
+        if (a > c)
+        {
+            mn = c;
+            cout << mn << " " << mx;
+        }
+        else
+        {
+            mn = a;
+            cout << mn << " " << mx;
+        }
+    }
     else
-        cout << c;
+    {
+        mx = c;
+        if (a > b)
+        {
+            mn = b;
+            cout << mn << " " << mx;
+        }
+        else
+        {
+            mn = a;
+            cout << mn << " " << mx;
+        }
+    }
     return 0;
 }
