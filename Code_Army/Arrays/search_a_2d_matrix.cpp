fa6e62bce4f36fab93cb8939ffca382f35e40d97
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-03 01:42:32 +0600

    search a 2d matrix resolved

--- Code_Army/Arrays/search_a_2d_matrix.cpp
+++ Code_Army/Arrays/search_a_2d_matrix.cpp
@@ -42,29 +42,28 @@ public:
         return false;
     }
 }; */
-/*
-third way
-O(logm*n)
-class Solution {
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int>> &matrix,int target){
-        int rows=matrix.size(),cols=matrix[0].size();
-        int low=0,high=rows*cols-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(matrix[mid/cols][mid%cols]==target){
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        int l = 0, r = m * n - 1;
+
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            int row = mid / n, col = mid % n;
+            if (matrix[row][col] == target)
                 return true;
-            }
-            else if(matrix[mid/cols][mid%cols]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            if (matrix[row][col] > target)
+                r = mid - 1;
+            else
+                l = mid + 1;
         }
         return false;
     }
-}; */
+};
 int main()
 {
     int m, n;
