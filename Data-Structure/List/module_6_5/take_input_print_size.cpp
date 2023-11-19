69f55b6e993a7440f28237679c62aa5f32f41fe5
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-20 02:28:53 +0600

    singly linked list take input and print size solved

--- Data-Structure/List/module_6_5/take_input_print_size.cpp
+++ Data-Structure/List/module_6_5/take_input_print_size.cpp
@@ -1,16 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+int sz = 0;
+void insert_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        sz++;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    sz++;
+}
 int main()
 {
-    int n;
-    cin >> n;
-    list<int> l;
-    while (n--)
+    Node *head = NULL;
+    int val;
+    while (true)
     {
-        int v;
-        cin >> v;
-        l.push_back(v);
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_tail(head, val);
     }
-    cout << l.size() << endl;
+    cout << sz << endl;
     return 0;
 }
