60eae3250c8cf594aba52ce29a26257ecce631e8
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-20 02:40:47 +0600

    check duplicates solved

---
+++ Data-Structure/List/module_6_5/check_duplicates.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+bool flag = false;
+void insert_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        if (tmp->val == val)
+            flag = true;
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+}
+int main()
+{
+    Node *head = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_tail(head, val);
+    }
+    flag ? cout << "YES\n" : cout << "NO\n";
+    return 0;
+}
