e894fe715ea5aa3b65cd418a8f23cd1d36146ef2
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-20 18:35:33 +0600

    optimal input operations added

--- Data-Structure/List/singly linked list/input.cpp
+++ Data-Structure/List/singly linked list/input.cpp
@@ -12,20 +12,34 @@ public:
     }
 };
 
-void insert_tail(Node *&head, int val)
+// ******* optimal O(1)
+void insert_tail(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
     if (head == NULL)
     {
         head = newNode;
+        tail = newNode;
         return;
     }
-    Node *tmp = head;
-    while (tmp->next != NULL)
-        tmp = tmp->next;
-
-    tmp->next = newNode;
+    tail->next = newNode;
+    tail = newNode;
 }
+//**********not optimal O(N)
+// void insert_tail(Node *&head, int val)
+// {
+//     Node *newNode = new Node(val);
+//     if (head == NULL)
+//     {
+//         head = newNode;
+//         return;
+//     }
+//     Node *tmp = head;
+//     while (tmp->next != NULL)
+//         tmp = tmp->next;
+
+//     tmp->next = newNode;
+// }
 void print_list(Node *head)
 {
     Node *tmp = head;
@@ -39,13 +53,14 @@ void print_list(Node *head)
 int main()
 {
     Node *head = NULL;
+    Node *tail = NULL;
     int val;
     while (true)
     {
         cin >> val;
         if (val == -1)
             break;
-        insert_tail(head, val);
+        insert_tail(head, tail, val);
     }
     print_list(head);
     return 0;
