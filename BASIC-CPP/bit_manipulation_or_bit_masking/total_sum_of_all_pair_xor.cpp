0e17a129a9aa41c90e589354778d68b2d028f1d0
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-21 18:01:36 +0600

    total sum of all pair xor problem solved

---
+++ BASIC-CPP/bit_manipulation_or_bit_masking/total_sum_of_all_pair_xor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ar[n + 1];
+
+    // Input array elements
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+    }
+
+    int ans = 0;
+
+    // Iterate through each bit position (0 to 30, since int is 32 bits)
+    for (int i = 0; i < 31; i++)
+    {
+        int one = 0, zero = 0;
+
+        // Count the number of elements with the i'th bit set and unset
+        for (int j = 0; j < n; j++)
+        {
+            if (ar[j] & (1 << i))
+                one++;
+            else
+                zero++;
+        }
+
+        // Calculate the count of pairs with i'th bit set and unset
+        int p = one * zero;
+
+        // Update the answer with the XOR of pairs having the i'th bit set
+        ans += (1 << i) * p;
+    }
+
+    // Print the final result
+    cout << ans << endl;
+
+    return 0;
+}
