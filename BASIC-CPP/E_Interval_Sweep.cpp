b7d382b2ad9c8d9e6d6b58419ea7a1126c4d6e37
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-30 19:12:49 +0600

    interval sweep problem solved

---
+++ BASIC-CPP/E_Interval_Sweep.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if ((a == 0 && b == 0) || (abs(a - b) >= 2))
+        cout << "NO";
+    else
+        cout << "YES";
+    return 0;
+}
