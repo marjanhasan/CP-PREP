e2c2f78b9e9e115148f33b4e0c4590bdc343609b
marjanhasan <moonmarjanhasan@gmail.com>
2023-11-30 19:37:21 +0600

    backspace string compare problem solved at leetcode

---
+++ Data-Structure/Stack/Module 15/backspace_string_compare.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        stack<char> st1, st2;
+        for (char c : s)
+        {
+            if (c == '#')
+            {
+                if (!st1.empty())
+                    st1.pop();
+            }
+            else
+                st1.push(c);
+        }
+        for (char c : t)
+        {
+            if (c == '#')
+            {
+                if (!st2.empty())
+                    st2.pop();
+            }
+            else
+                st2.push(c);
+        }
+
+        return st1 == st2;
+    }
+};
+int main()
+{
+
+    return 0;
+}
