7c0f65c013a534cf3313eded65b46c86ffa570e5
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-02 02:15:54 +0600

    same to same again problem solved from assignment

---
+++ Data-Structure/Stack/assignment/same_to_same_again.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myStack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void pop()
+    {
+        sz--;
+        Node *deleteNode = tail;
+        tail = tail->prev;
+        delete deleteNode;
+        if (tail != NULL)
+        {
+            tail->next = NULL;
+        }
+        else
+        {
+            head = NULL;
+        }
+    }
+
+    int top()
+    {
+        return tail->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        return sz == 0;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void pop()
+    {
+        sz--;
+        Node *deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+        else
+        {
+            head->prev = NULL;
+        }
+    }
+
+    int front()
+    {
+        return head->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        return sz == 0;
+    }
+};
+
+int main()
+{
+    // Write your code here
+    int n, m;
+    cin >> n >> m;
+    if (n != m)
+    {
+        cout << "NO";
+        return 0;
+    }
+    myStack st;
+    int val;
+    while (n--)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        st.push(val);
+    }
+
+    myQueue q;
+    while (m--)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        q.push(val);
+    }
+    bool flag = false;
+    while (!st.empty())
+    {
+        if (st.top() != q.front())
+        {
+            flag = true;
+            break;
+        }
+        st.pop();
+        q.pop();
+    }
+
+    flag ? cout << "NO" : cout << "YES";
+    return 0;
+}
