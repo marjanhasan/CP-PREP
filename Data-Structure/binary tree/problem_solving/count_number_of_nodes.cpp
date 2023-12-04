024a38770a51c563bf4b3cf63cbcdd72a8cc524e
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-04 19:09:39 +0600

    count number of nodes in binary tree solved

---
+++ Data-Structure/binary tree/problem_solving/count_number_of_nodes.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *left, *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int count_node(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int left = count_node(root->left);
+    int right = count_node(root->right);
+    return left + right + 1;
+}
+int main()
+{
+    Node *root = input_tree();
+    cout << count_node(root);
+    return 0;
+}
