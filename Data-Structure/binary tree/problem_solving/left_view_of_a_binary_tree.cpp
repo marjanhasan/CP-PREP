804233217ee5320f207972afd0259e0f7bf4158f
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-06 17:55:24 +0600

    left view of a binary tree

---
+++ Data-Structure/binary tree/problem_solving/left_view_of_a_binary_tree.cpp
@@ -0,0 +1,28 @@
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    //    Write your code here
+    bool frq[3005] = {false};
+    queue<pair<TreeNode<int> *, int>> q;
+    if (root)
+        q.push({root, 1});
+    vector<int> v;
+    while (!q.empty())
+    {
+        pair<TreeNode<int> *, int> pr = q.front();
+        q.pop();
+        TreeNode<int> *node = pr.first;
+        int level = pr.second;
+
+        if (!frq[level])
+        {
+            v.push_back(node->data);
+            frq[level] = true;
+        }
+
+        if (node->left)
+            q.push({node->left, level + 1});
+        if (node->right)
+            q.push({node->right, level + 1});
+    }
+    return v;
+}
