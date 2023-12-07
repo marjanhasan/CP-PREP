8cda0c07bcb6b14f8bfea8fb6733d8d25e20d33f
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-07 20:19:40 +0600

    max min leaf solved from assignment

---
+++ Data-Structure/binary tree/assignment/max_min_leaf.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int mn = INT_MAX, mx = INT_MIN;
+void traverse(Node *root)
+{
+    if (!root)
+        return;
+    if (!root->left && !root->right)
+    {
+        if (mn > root->val)
+            mn = root->val;
+        if (mx < root->val)
+            mx = root->val;
+    }
+    traverse(root->left);
+    traverse(root->right);
+}
+Node *input()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *left, *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        p->left = left;
+        p->right = right;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int main()
+{
+    Node *root = input();
+    traverse(root);
+    cout << mx << " " << mn;
+    return 0;
+}
