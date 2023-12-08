ef089b6ec1cea687e1d2285e4a055a69a21dddad
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-08 19:40:18 +0600

    800 Rated problem solved from codeforces

---
+++ BASIC-CPP/A_Game_with_Integers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 3 == 1 || n % 3 == 2)
+            cout << "First\n";
+        else if (n % 3 == 0)
+            cout << "Second\n";
+    }
+    return 0;
+}
