24c11eecb9dd7d00d864cfe369e2ffa9a443b8c4
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-12 21:12:48 +0600

    min heap deletion recap done

---
+++ Data-Structure/Heap/min_heap_deletion.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insert_heap(vector<int> &v, int x)
+{
+    v.push_back(x);
+    int cur_idx = v.size() - 1;
+
+    while (cur_idx != 0)
+    {
+        /* code */
+        int parent = (cur_idx - 1) / 2;
+        if (v[parent] > v[cur_idx])
+            swap(v[parent], v[cur_idx]);
+        else
+            break;
+        cur_idx = parent;
+    }
+}
+
+void delete_heap(vector<int> &v)
+{
+    v[0] = v[v.size() - 1];
+    v.pop_back();
+    int cur = 0;
+    while (true)
+    {
+        /* code */
+        int left_idx = cur * 2 + 1;
+        int right_idx = cur * 2 + 2;
+        int last_idx = v.size() - 1;
+        if (left_idx <= last_idx && right_idx <= last_idx)
+        {
+            if (v[left_idx] <= v[right_idx] && v[left_idx] < v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else if (v[right_idx] <= v[left_idx] && v[right_idx] < v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+                break;
+        }
+        else if (left_idx <= last_idx)
+        {
+            if (v[left_idx] < v[cur])
+            {
+                swap(v[left_idx], v[cur]);
+                cur = left_idx;
+            }
+            else
+                break;
+        }
+        else if (right_idx <= last_idx)
+        {
+            if (v[right_idx] < v[cur])
+            {
+                swap(v[right_idx], v[cur]);
+                cur = right_idx;
+            }
+            else
+                break;
+        }
+        else
+            break;
+    }
+}
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        insert_heap(v, x);
+    }
+    delete_heap(v);
+    // delete_heap(v);
+    // delete_heap(v);
+    // delete_heap(v);
+    for (int val : v)
+        cout << val << " ";
+    return 0;
+}
