62ceecab43d3207375cfe037c437fb7850f22bde
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-16 18:04:48 +0600

    maximum product of two elements solved

---
+++ Data-Structure/Vector or Arrays Operation/max_product_two_element.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int maxProduct(vector<int> &v)
+    {
+        sort(v.begin(), v.end());
+        int i = v.size() - 1, j = v.size() - 2;
+        return ((v[i] - 1) * (v[j] - 1));
+    }
+};
