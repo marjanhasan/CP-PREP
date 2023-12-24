850ba9c7d65fb8581c3b5901ec743986f08fc2f4
marjanhasan <moonmarjanhasan@gmail.com>
2023-12-24 17:58:27 +0600

    codeforces rated problem solved

---
+++ A_Shaass_and_Oskols.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n + 1] = {0};
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    int m;
+    cin >> m;
+    int x, y;
+    while (m--)
+    {
+        cin >> x >> y;
+        a[x - 1] += y - 1;
+        a[x + 1] += a[x] - y;
+        a[x] = 0;
+    }
+    for (int i = 1; i <= n; i++)
+        cout << a[i] << endl;
+    return 0;
+}
