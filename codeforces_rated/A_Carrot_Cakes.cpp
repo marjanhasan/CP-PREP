924af05de87699fbcd9a16799e33e16a36e1f154
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-19 00:01:32 +0600

    carrot cakes solved after a month

--- codeforces_rated/A_Carrot_Cakes.cpp
+++ codeforces_rated/A_Carrot_Cakes.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define nl '\n'
 int main()
 {
-    int n, t, k, d;
-    cin >> n >> t >> k >> d;
-    (floor((t * n) / k) > (t + d)) ? cout << "YES" : cout << "NO";
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    ll y = (d / b) + 1;
+
+    (y * c < a) ? cout << "YES\n" : cout << "NO\n";
     return 0;
 }
