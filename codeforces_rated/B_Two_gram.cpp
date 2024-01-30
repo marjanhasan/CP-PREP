eab2fcf1173963108a647159f1b852e5410d56d1
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-30 17:14:03 +0600

    codeforces rated problem solved

--- codeforces_rated/B_Two_gram.cpp
+++ codeforces_rated/B_Two_gram.cpp
@@ -12,54 +12,24 @@ int main()
     cin >> n;
     string s;
     cin >> s;
-    vector<pair<string, int>> v;
-    int sz = s.size();
-    bool odd = false;
-    if (sz % 2 == 1)
-        odd = true;
-    string con;
-    bool flag = false;
-    for (int i = 0; i < sz; i += 2)
-    {
-        con = "";
-        con += s[i];
-        con += s[i + 1];
-        flag = false;
-        for (int j = 0; j < v.size(); j++)
-        {
-            if (v[j].first == con)
-                v[j].second++;
-            flag = true;
-        }
-        if (!flag)
-            v.push_back({con, 1});
-    }
-    if (odd)
-    {
-        con = "";
-        con += s[n - 2];
-        con += s[n - 1];
-    }
-    flag = false;
-    for (int j = 0; j < v.size(); j++)
-    {
-        if (v[j].first == con)
-            v[j].second++;
-        flag = true;
-    }
-    if (!flag)
-        v.push_back({con, 1});
+    map<string, int> mp;
     int mx = 0;
-    for (int i = 0; i < v.size(); i++)
+    string a;
+    for (int i = 0; i < n - 1; i++)
     {
-        if (mx < v[i].second)
-        {
-            con = "";
-            con += v[i].first;
-            mx = v[i].second;
-        }
-        // cout << v[i].first << " " << v[i].second << nl;
+        a = "";
+        a += s[i];
+        a += s[i + 1];
+        mp[a]++;
+        mx = max(mx, mp[a]);
     }
-    cout << con;
+    for (auto x : mp)
+    {
+        if (x.second == mx)
+        {
+            cout << x.first;
+            break;
+        }
+    }
     return 0;
 }
