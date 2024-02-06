0768df43aaafa3a43525909607895ff8e12b8322
marjanhasan <moonmarjanhasan@gmail.com>
2024-02-07 03:54:45 +0600

    mst problem solved

---
+++ Algorithm/MST/Road_Reparation.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+class Edge
+{
+public:
+    ll u, v, w;
+    Edge(ll u, ll v, ll w)
+    {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+};
+bool cmp(Edge a, Edge b)
+{
+    return a.w < b.w;
+}
+const int N = 1e5 + 5;
+int parent[N];
+int group_size[N];
+
+void dsu_initialize(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        group_size[i] = 1;
+    }
+}
+int dsu_find(int node)
+{
+    if (parent[node] == -1)
+        return node;
+    return parent[node] = dsu_find(parent[node]);
+}
+void dsu_union_by_size(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if (group_size[leaderA] > group_size[leaderB])
+    {
+        parent[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA];
+    }
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, e;
+    cin >> n >> e;
+    dsu_initialize(n);
+    vector<Edge> edgeList;
+    while (e--)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edgeList.push_back(Edge(u, v, w));
+    }
+    sort(edgeList.begin(), edgeList.end(), cmp);
+    ll totalCost = 0;
+    for (Edge ed : edgeList)
+    {
+        int leaderU = dsu_find(ed.u);
+        int leaderV = dsu_find(ed.v);
+        if (leaderU != leaderV)
+        {
+            dsu_union_by_size(ed.u, ed.v);
+            totalCost += ed.w;
+        }
+    }
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (parent[i] == -1)
+        {
+            cnt++;
+        }
+    }
+    cnt == 1 ? cout << totalCost : cout << "IMPOSSIBLE";
+    return 0;
+}
