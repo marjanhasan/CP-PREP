e87defaf0e3104f27f47916e2661ad8456ce30e8
marjanhasan <moonmarjanhasan@gmail.com>
2024-02-07 01:12:32 +0600

    cycle detection using dsu recap done

---
+++ Algorithm/DSU/cycle_detection.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+const int N = 1e5 + 5;
+int parent[N];
+int group_size[N];
+void dsu_initialize(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = -1;
+        group_size[i] = 1;
+    }
+}
+int dsu_find(int node)
+{
+    if (parent[node] == -1)
+        return node;
+    return parent[node] = dsu_find(parent[node]);
+}
+void dsu_union_by_size(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if (group_size[leaderA] > group_size[leaderB])
+    {
+        parent[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA];
+    }
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, e;
+    cin >> n >> e;
+    dsu_initialize(n);
+    bool flag = false;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int leaderA = dsu_find(a);
+        int leaderB = dsu_find(b);
+        if (leaderA == leaderB)
+            flag = true;
+        else
+            dsu_union_by_size(a, b);
+    }
+    flag ? cout << "Cycle found" : cout << "Cycle Not found";
+    return 0;
+}
