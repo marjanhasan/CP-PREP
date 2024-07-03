5719affd6313f6884ea36a57d2415002295c8c64
marjanhasan <moonmarjanhasan@gmail.com>
2024-07-03 12:08:17 +0600

    task done

---
+++ codeforces_rated/A_Again_Twenty_Five.cpp
@@ -0,0 +1,31 @@
+/*
+ * author: marjanhasan
+ * created: 2024-07-03 11:56:51
+ * problem link: 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define pii pair<ll, ll>
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << 25;
+}
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T=1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
