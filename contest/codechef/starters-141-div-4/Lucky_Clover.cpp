b206ab0f2313e7981f3acb2d5e7165472f8b0226
marjanhasan <moonmarjanhasan@gmail.com>
2024-07-03 21:33:29 +0600

    contest participation done

---
+++ contest/codechef/starters-141-div-4/Lucky_Clover.cpp
@@ -0,0 +1,23 @@
+/*
+* author: marjanhasan
+* created: 2024-07-03 20:32:51
+* problem link: 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl  '\n'
+#define pii pair<ll,ll>
+int main()
+{
+    ios_base :: sync_with_stdio (false);
+    cin.tie(0);
+    cout.tie(0);
+    
+
+
+    int n;
+    cin >> n;
+    cout << 4 + ((n-1) * 3);
+    return 0;
+}
