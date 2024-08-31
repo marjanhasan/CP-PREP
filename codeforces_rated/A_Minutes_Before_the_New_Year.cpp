64ced873716702fef95bf1a16012ff6eb7859107
marjanhasan <moonmarjanhasan@gmail.com>
2024-08-31 15:47:26 +0600

    task done

---
+++ codeforces_rated/A_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,31 @@
+/*
+ * author: marjanhasan
+ * created: 2024-08-31 14:57:07
+ * problem link:
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define pii pair<ll, ll>
+
+void solve()
+{
+  int h, m;
+  cin >> h >> m;
+  cout << ((24 - h) * 60) - m << nl;
+}
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    solve();
+  }
+  return 0;
+}
