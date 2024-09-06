0234a63a3f02ddae17290ba3ca53f0cb07f02a84
marjanhasan <moonmarjanhasan@gmail.com>
2024-09-06 16:12:22 +0600

    task done

---
+++ codeforces_rated/B_Good_Kid.cpp
@@ -0,0 +1,39 @@
+/*
+ * author: marjanhasan
+ * created: 2024-09-06 15:38:05
+ * problem link:
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define pii pair<ll, ll>
+
+void solve()
+{
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  v[0] += 1;
+  int prod = 1;
+  for (int i = 0; i < n; i++)
+    prod *= v[i];
+  cout << prod << nl;
+}
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    solve();
+  }
+  return 0;
+}
