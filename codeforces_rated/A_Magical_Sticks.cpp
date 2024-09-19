c2c3b7695f4a3714cc52e70f0ef460c5ebec3f89
marjanhasan <moonmarjanhasan@gmail.com>
2024-09-19 16:22:56 +0600

    task done

---
+++ codeforces_rated/A_Magical_Sticks.cpp
@@ -0,0 +1,31 @@
+/*
+ * author: marjanhasan
+ * created: 2024-09-19 15:56:33
+ * problem link:
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define pii pair<ll, ll>
+
+void solve()
+{
+  int n;
+  cin >> n;
+  cout << (n + 1) / 2 << nl;
+}
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    solve();
+  }
+  return 0;
+}
