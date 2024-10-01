365d1b5d33cb08fbd2f2cc2aa9d306f82aa89ea5
marjanhasan <moonmarjanhasan@gmail.com>
2024-10-01 06:30:52 +0600

    Task done

---
+++ codeforces_rated/B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,31 @@
+/*
+ * author: marjanhasan
+ * created: 2024-10-01 06:28:41
+ * problem link:
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define pii pair<ll, ll>
+
+void solve()
+{
+  int n;
+  cin >> n;
+  cout << (n == 3 ? 3 : 2) << nl;
+}
+int main()
+{
+  ios_base ::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    solve();
+  }
+  return 0;
+}
